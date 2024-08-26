8b1a0f06858a712e0ed806ee8362a7dadd2c050b
Sankalp <sankalpchauhan.me@gmail.com>
2024-08-26 12:54:02 +0530

    fix: #4 added tests to ensure MapSeed implements createCompositeProperty

--- src/test/kotlin/exercise/Map.kt
+++ src/test/kotlin/exercise/Map.kt
@@ -6,12 +6,21 @@ import kia.jkid.serialization.serialize
 import kotlin.test.Ignore
 import kotlin.test.assertEquals
 
+data class Book(val title: String, val price: Double)
 data class BookStore(val bookPrice: Map<String, Double>)
+data class Library(val sections: Map<String, Book>)
 
 @Ignore
 class MapTest {
     private val bookStore = BookStore(mapOf("Catch-22" to 10.92, "The Lord of the Rings" to 11.49))
     private val json = """{"bookPrice": {"Catch-22": 10.92, "The Lord of the Rings": 11.49}}"""
+    private val library = Library(
+        mapOf(
+            "fiction" to Book("Catch-22", 10.92),
+            "fantasy" to Book("The Lord of the Rings", 11.49)
+        )
+    )
+    private val libraryJson = """{"sections": {"fiction": {"price": 10.92, "title": "Catch-22"}, "fantasy": {"price": 11.49, "title": "The Lord of the Rings"}}}"""
 
     @Test fun testSerialization() {
         println(serialize(bookStore))
@@ -20,5 +29,14 @@ class MapTest {
 
     @Test fun testDeserialization() {
         assertEquals(bookStore, deserialize(json))
+    }
+
+    @Test fun testObjectMapSerialization() {
+        println(serialize(library))
+        assertEquals(libraryJson, serialize(library))
+    }
+
+    @Test fun testObjectMapDeserialization() {
+        assertEquals(library, deserialize<Library>(libraryJson))
     }
 }
